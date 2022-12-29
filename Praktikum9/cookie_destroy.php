e17851a811e057763fc3e74c184b40aa11c97072
Nidasakinaa <114220455+Nidasakinaa@users.noreply.github.com>
2022-12-29 16:33:57 +0700

    Add files via upload

---
+++ Praktikum9/cookie_destroy.php
@@ -0,0 +1,4 @@
+<?php
+setcookie("cookie_no_induk","");
+echo("No Induk dalam cooies sudah dihapus !");
+?>
---
+++ Praktikum9/loggin.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+
+if(isset($HTTP_POST_VARS["insert"]))
+{
+    if(($HTTP_POST_VARS["user"]!="")&&($HTTP_POST_VARS["password"]!=""))
+    {
+        session_register("session_status");
+        session_register("session_user");
+        session_register("session_password");
+        $session_status="login";
+        $session_user=$HTTP_POST_VARS["user"];
+        $session_password=$HTTP_POST_VARS["password"];
+        header("Location : proses_loggin.php");
+        exit;
+    }
+}?>
+<html>
+    <head></head>
+    <body>
+        <h2>Penggunaan Session </h2>
+        <pre>
+            <form action="loggin.php" method="post">
+                User name       : <input type="text" name="user" size="15">
+                User password   : <input type="password" name="password" size="15">
+                                  <input type="hidden" name="insert">
+                                  <input type="submit" name="loggin" value="Loggin">
+            </form>
+        </pre>
+    </body>
+</html>
